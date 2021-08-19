0d32cd8e63cb268ed98034cea5d003de2bbf8746
LZT <1610827367@qq.com>
2021-08-19 16:33:38 +0800

    commit test

---
+++ src/test/gitTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author LZT
+ * @date 2021/8/19
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
